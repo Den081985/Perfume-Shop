4de221765e64a7f6d4b68be6c30beb655d02d8e0
Den081985 <pishnenkod@gmail.com>
2022-01-02 15:11:03 +0300

    edit localStoregeUtil

--- Utils/localStorageUtil.js
+++ Utils/localStorageUtil.js
@@ -1,35 +1,34 @@
 class LocalStorageUtil {
-    constructor() {
-        this.keyName = "products";
+  constructor() {
+    this.keyName = "products";
+  }
+
+  getProducts() {
+    const productsLocalStorage = localStorage.getItem(this.keyName);
+
+    if (productsLocalStorage !== null) {
+      return JSON.parse(productsLocalStorage);
+    }
+    return [];
+  }
+
+  putProducts(id) {
+    let products = this.getProducts();
+    let pushProduct = false;
+
+    const index = products.indexOf(id);
+
+    if (index === -1) {
+      products.push(id);
+      pushProduct = true;
+    } else {
+      products.splice(index, 1);
     }
 
-    getProducts() {
-        const productsLocalStorage = localStorage.getItem(this.keyName);
+    localStorage.setItem(this.keyName, JSON.stringify(products));
 
-        if(productsLocalStorage !== null) {
-            return JSON.parse(productsLocalStorage);
-        }
-        return [];
-    }
-
-    putProducts(id) {
-        let products = this.getProducts();
-        let pushProduct = false;
-
-        const index = products.indexOf(id); 
-
-        if(index === -1) {
-            products.push(id);
-            pushProduct = true;
-        } else {
-            products.splice(index, 1);
-        }
-       
-        localStorage.setItem(this.keyName, JSON.stringify(products));
-
-        return {pushProduct , products}
-    }
+    return { pushProduct, products };
+  }
 }
 
 const localStorageUtil = new LocalStorageUtil();
-
