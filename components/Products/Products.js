61175a006de8513a411791bee3582c87c52eb941
Den081985 <pishnenkod@gmail.com>
2022-01-02 17:27:49 +0300

    add onclick to button and handleSetStorage

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,24 +1,55 @@
 class Products {
-    render() {
-        let htmlCatalog = ``;
+  constructor() {
+    this.classNameActive = "products-element-btn-active";
+    this.labelAdd = "Добавить в кoрзину";
+    this.labelRemove = "Удалить из корзины";
+  }
 
-        CATALOG.forEach(({id, name, price, img}) => {
-            htmlCatalog += `
+  handleSetStorage(element, id) {
+    const { pushProduct, products } = localStorageUtil.putProducts(id);
+    if (pushProduct) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelRemove;
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+    }
+  }
+
+  render() {
+    const productsStore = localStorageUtil.getProducts();
+
+    let htmlCatalog = ``;
+
+    CATALOG.forEach(({ id, name, price, img }) => {
+      let activeClass = "";
+      let activeText = "";
+
+      if (productsStore.indexOf(id) === -1) {
+        activeText = this.labelAdd;
+      } else {
+        activeText = this.labelRemove;
+        activeClass = " " + this.classNameActive;
+      }
+
+      htmlCatalog += `
                 <li class = "products-element">
                     <span class = "products-element-name">${name}</span>
                     <img class = "products-element-img" src = "${img}"/>
                     <span class = "products-element-price">Цена: ${price.toLocaleString()}P</span>
-                    <button class = "products-element-btn">Добавить в кoрзину</button>
+                    <button class = "products-element-btn${activeClass}" onclick = "productsPage.handleSetStorage(this,'${id}');">
+                    ${activeText}
+                    </button>
                 </li>
             
-            `
-            const html = `
+            `;
+      const html = `
                 <ul class = "products-container">${htmlCatalog}</ul>
             
-            `
-            ROOT_PRODUCTS.innerHTML = html;
-        })
-    }
+            `;
+      ROOT_PRODUCTS.innerHTML = html;
+    });
+  }
 }
 
 const productsPage = new Products();
