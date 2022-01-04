6d97c18e92a22be4a38208be37220b82c4769053
Den081985 <pishnenkod@gmail.com>
2022-01-04 19:19:11 +0300

    add headerPage.handleOpenShopPage method

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,13 @@
 class Header {
+  handleOpenShopPage() {
+    shoppingPage.render();
+  }
+
   render(counter) {
     const html = `
         <div class = "header-container">
             <span class = "header-span">Магазин Парфюмерии</span>
-            <div class = "header-counter">
+            <div class = "header-counter" onclick = "headerPage.handleOpenShopPage();">
                Товаров в корзине: ${counter}
             </div>
         </div>
