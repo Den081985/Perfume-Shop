5e20a032543c2f8785181d5c9c02769c74ccd44c
Den081985 <pishnenkod@gmail.com>
2022-01-08 22:03:22 +0300

    add form to Shopping.render

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -23,17 +23,49 @@ class Shopping {
     });
 
     const html = `
-        <div class = "shopping-container">
-          <div class = "shopping-close" onclick = "shoppingPage.handleClear();"></div>
-          <table>
-          ${htmlCatalog}
-                <tr>
+        <div class = "shopping-wrapper">
+          
+            <div class = "shopping-close" onclick = "shoppingPage.handleClear();"></div>
+              <div class = "shopping-list">
+                <table>
+                 ${htmlCatalog}
+                  <tr>
                     <td class = "shopping-name">Сумма:</td>
                     <td class = "shopping-price">${sumCatalog.toLocaleString()}Р</td>
-                </tr>
-          </table>
-
-         
+                  </tr>
+                </table>
+              </div>
+          
+            <div class = "shopping-form">
+              <form>
+                <span class = "shopping-span">Оформить заказ</span>
+                <p class = "shopping-input">
+                  <label for="firstname">Имя</label>
+                  <input type="text" id="firstname" name="user_name">
+                </p>
+                <p class = "shopping-input">
+                  <label for="lastname">Фамилия</label>
+                  <input type="text" id="lastname" name="user_lastname">
+                </p>
+                <p class = "shopping-input">
+                  <label for="address">Адрес</label>
+                  <input type="text" id="address" name="user_address">
+                </p>
+                <p class = "shopping-input">
+                  <label for="email">E-mail</label>
+                  <input type="email" id="email" name="user_email">
+                </p>
+                <p class = "shopping-input">
+                  <label for="tel">Телефон</label>
+                  <input type="tel" id="tel" name="user_tel">
+                </p>
+                <div class = "shopping-btnContainer">
+                <button  type="submit">Оформить заказ</button>
+              </div>
+              </form>
+              
+            </div>
+            
         </div>
     `;
     ROOT_SHOPPING.innerHTML = html;
